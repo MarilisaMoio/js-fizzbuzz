1c2cecb253a4ea5aa4130795f4b0e6529b37d8cf
MarilisaMoio <moiomarilisa@gmail.com>
2024-03-07 23:00:35 +0100

    fix: eliminate useless indentation

--- js/script.js
+++ js/script.js
@@ -1,31 +1,29 @@
 //milestone 1: basic FizzBuzz; milestone 2: aggiungere gli elementi nel DOM; milestone 3: aggiungere stile.
 
 //scrivere un ciclo for che fornisca un numero da 1 a 100
-    for (let i = 1; i <= 100; i++){
+for (let i = 1; i <= 100; i++){
 
-        //recupera il container dall'html
-        const container = document.querySelector(".container");
+    //recupera il container dall'html
+    const container = document.querySelector(".container");
 
-        //condizioni per fizz, buzz e fizzbuzz
-        let print = i;
+    //condizioni per fizz, buzz e fizzbuzz
+    let print = i;
 
-        const newDiv = document.createElement("div");
+    const newDiv = document.createElement("div");
 
-        console.log(newDiv)
-
-        if (i % 3 === 0 && i % 5 === 0){
-            print = "FizzBuzz";
-            newDiv.classList.add("orange");
-        } else if (i % 3 === 0 ) {
-            print = '<img src="img/fizz.webp" alt="fizz">'
-        } else if (i % 5 === 0 ) {
-            print = '<img src="img/buzz.webp" alt="buzz">'
-        }
-
-        //prepara il div
-        newDiv.innerHTML = print;
-
-        container.append(newDiv);
+    if (i % 3 === 0 && i % 5 === 0){
+        print = "FizzBuzz";
+        newDiv.classList.add("orange");
+    } else if (i % 3 === 0 ) {
+        print = '<img src="img/fizz.webp" alt="fizz">'
+    } else if (i % 5 === 0 ) {
+        print = '<img src="img/buzz.webp" alt="buzz">'
     }
+
+    //prepara il div
+    newDiv.innerHTML = print;
+
+    container.append(newDiv);
+}
 
     
